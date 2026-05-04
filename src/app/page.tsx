0ce00cd42172006f4aea1fb9302a94bed71dc145
Patryk <patorzech980202@gmail.com>
2026-05-04 21:44:34 +0200

    update feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,10 +58,10 @@ export default function Home() {
 
   // 🔥 DEPARTAMENTY (POKOJOWE/ SZEFOWA)
   const departments = [
-    { id: 1, name: "POKOJOWE" },
-    { id: 2, name: "SZEFOWA" }
-    { id: 3, name: "RECEPCJA" }
-  ]
+  { id: 1, name: "POKOJOWE" },
+  { id: 2, name: "SZEFOWA" },
+  { id: 3, name: "RECEPCJA" }
+]
 
   // 🔐 INIT USER
   useEffect(() => {
