cc211f9af3328d52711bbee8064bfe1836c93774
Patryk <patorzech980202@gmail.com>
2026-05-27 11:16:45 +0200

    fix push subscription

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
 "use client"
 
+
 import { useEffect, useState } from "react"
 import { supabase } from "../../lib/supabase"
+
+function urlBase64ToUint8Array(base64String: string) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4)
+
+  const base64 = (base64String + padding)
+    .replace(/-/g, "+")
+    .replace(/_/g, "/")
+
+  const rawData = window.atob(base64)
+
+  const outputArray = new Uint8Array(rawData.length)
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+
+  return outputArray
+}
 
 type Task = {
   id: number
@@ -312,9 +331,9 @@ export default function Home() {
     }
 
     const subscription = await registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: publicKey,
-    })
+  userVisibleOnly: true,
+  applicationServerKey: urlBase64ToUint8Array(publicKey),
+   })
 
     const { data: auth } = await supabase.auth.getUser()
 
